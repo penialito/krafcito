0d73bc54a5eb9b318064e3afea2fb22445da5231
penialito <43260174+penialito@users.noreply.github.com>
2025-01-31 18:52:17 -0300

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,1 +1,8 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
+const firebaseConfig = { /* Tu configuración */ };
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, signInWithEmailAndPassword };
