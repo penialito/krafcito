ed0354caf916107e151f210c2c31418da0f19858
penialito <43260174+penialito@users.noreply.github.com>
2025-02-28 02:29:49 -0300

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,13 +1,17 @@
-// Event listener for the login form submission
-document.getElementById('loginForm')?.addEventListener('submit', async (e) => {
-    e.preventDefault();
+// scripts/auth.js
 
-        // Initialize the Supabase client with your project URL and anon key
+const initializeSupabase = async () => {
     const { createClient } = Supabase; // Import createClient
     const supabaseUrl = 'https://gztsbqbqmesfrvywpyhl.supabase.co';
     const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6dHNicWJxbWVzZnJ2eXdweWhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyMDg3NDUsImV4cCI6MjA1NDc4NDc0NX0.EmRDO3s64iYw1k3OY5W44twraLnJHy6bQh3HKTtx-wI';
-    const supabase = window.supabase = createClient(supabaseUrl, supabaseAnonKey);
+    return createClient(supabaseUrl, supabaseAnonKey);
+};
 
+const loginForm = document.getElementById('loginForm');
+
+loginForm?.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const supabase = await initializeSupabase();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
