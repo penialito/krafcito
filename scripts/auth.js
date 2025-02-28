63747ab426a469bc2f300d278fdb45a01b758c60
penialito <43260174+penialito@users.noreply.github.com>
2025-02-28 01:31:03 -0300

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,5 +1,11 @@
 // Initialize the Supabase client with your project URL and anon key
-const supabase = window.supabase = supabase.createClient('https://gztsbqbqmesfrvywpyhl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6dHNicWJxbWVzZnJ2eXdweWhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyMDg3NDUsImV4cCI6MjA1NDc4NDc0NX0.EmRDO3s64iYw1k3OY5W44twraLnJHy6bQh3HKTtx-wI');
+const { createClient } = Supabase; // Import createClient
+
+const supabaseUrl = 'https://gztsbqbqmesfrvywpyhl.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6dHNicWJxbWVzZnJ2eXdweWhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyMDg3NDUsImV4cCI6MjA1NDc4NDc0NX0.EmRDO3s64iYw1k3OY5W44twraLnJHy6bQh3HKTtx-wI';
+
+const supabase = window.supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 
 // Event listener for the login form submission
 document.getElementById('loginForm')?.addEventListener('submit', async (e) => {
